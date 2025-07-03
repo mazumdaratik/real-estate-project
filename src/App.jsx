f398cac052e2f37a1a34c4bf0ad456f20b4233bd
Atik Mazumdar <rjatik990@gmail.com>
2025-07-04 01:48:32 +0600

    Home added to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 
 import './App.css'
-
+import Home from './components/home/Home'
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
 
 function App() {
   
 
   return (
     <div>
-
+    <Navbar/>
+    <Home />
     </div>
   )
 }
