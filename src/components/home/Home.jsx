a5dbb71ab0300b39e6ede8123887eab1abb26310
Atik Mazumdar <rjatik990@gmail.com>
2025-07-04 01:46:41 +0600

    two component imported

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import PropertyType from './PropertyType'
+import PropertyRole from './PropertyRole'
+
+const Home = () => {
+
+  return (
+    <div className='m-4'>
+        <PropertyType />
+        <PropertyRole/>
+    </div>
+  )
+}
+
+export default Home
