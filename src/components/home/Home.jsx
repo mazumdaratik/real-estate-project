e32caf111d66955d4020fe88df514724b1de339c
Atik Mazumdar <rjatik990@gmail.com>
2025-07-05 00:14:37 +0600

    state declared ans passed to compponents

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, {useState}from 'react'
 
 import PropertyType from './PropertyType'
 import PropertyRole from './PropertyRole'
+import ProofOwnership from './ProofOwnership'
+import RealtorVerification from './RealtorVerification'
+import CompanyInfo from './CompanyInfo'
 
 const Home = () => {
-
+    const[selectPropertyType, setSelectPropertyType] = useState(null)
+    const[selectPropertyRole, setSelectPropertyRole] = useState(null)
   return (
     <div className='m-4'>
-        <PropertyType />
-        <PropertyRole/>
+        <PropertyType selectPropertyType={selectPropertyType} setSelectPropertyType={setSelectPropertyType}/>
+        <PropertyRole selectPropertyRole={selectPropertyRole} setSelectPropertyRole={setSelectPropertyRole}/>
+        <ProofOwnership selectPropertyType={selectPropertyType} selectPropertyRole={selectPropertyRole}/>
+        <RealtorVerification selectPropertyType={selectPropertyType} selectPropertyRole={selectPropertyRole} />
+        <CompanyInfo selectPropertyType={selectPropertyType} selectPropertyRole={selectPropertyRole} />
     </div>
   )
 }
