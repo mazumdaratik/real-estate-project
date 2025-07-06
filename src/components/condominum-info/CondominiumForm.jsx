e9fafc615631fd640de768bddca2279c031b493d
Atik Mazumdar <rjatik990@gmail.com>
2025-07-07 00:38:50 +0600

    simplified component

--- src/components/condominum-info/CondominiumForm.jsx
+++ src/components/condominum-info/CondominiumForm.jsx
@@ -1,61 +1,16 @@
-// File: CondominiumForm.jsx
-
 import React from "react";
-import { TbArrowDownFromArc } from "react-icons/tb";
-
-const fieldData = [
-  { label: "Property address", required: true },
-  { label: "Pet fees", optional: true, note: ", (add fees if you allow pet)" },
-  { label: "Leasing info", required: true },
-  { label: "Parking", optional: true },  
-  { label: "Charges", required: true },
-  { label: "Nearest educational institution", optional: true, recommended: true },
-  { label: "Rent frequency & payment reminder", required: true },
-  { label: "Application agreement", optional: true },
-  { label: "About the property", optional: true },
-  { label: "Community’s amenity/features", optional: true, recommended: true },
-  { label: "Nearest stations", optional: true, recommended: true },
-  { label: "Nearest landmark", optional: true, recommended: true },
-  { label: "Utilities provider", optional: true, recommended: true },
-];
+import PhotoUpload from "./PhotoUpload";
+import Form from "./Form";
+import UploadVideos from "./UploadVideos";
 
 const CondominiumForm = () => {
+
   return (
     <div className="p-6 bg-white rounded-md shadow-sm">
       <h2 className="text-xl font-semibold px-5 mb-6">Condominiums information</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2">
-        {
-          fieldData.map( (field, index) => (
-            <div key={index}
-              className="flex justify-between items-center m-4 px-4 py-3 border border-gray-300 rounded-2xl "
-            >
-                <div>
-                  <p className="font-semibold text-lg py-1">
-                    {field.label}
-                    {field.required && (
-                      <span className="text-red-500"> (Required)</span>
-                    )}
-                    {field.optional && !field.recommended && (
-                      <span className="text-gray-600"> (optional)</span>
-                    )}
-                    {field.optional && field.recommended && (
-                      <span className="text-gray-600"> (optional but recommended)</span>
-                    )}
-                    {field.note && (
-                      <span className="text-gray-600"> {field.note}</span>
-                    )}
-                    
-                  </p>
-                </div>
-
-                <button className="flex justify-center items-center gap-1">
-                  <span className="text-base">+</span>
-                  <span className="underline cursor-pointer">Add</span>
-                </button>
-            </div>
-          ))
-        }
-      </div>
+        <Form />
+        <PhotoUpload/>
+        <UploadVideos/>
     </div>
   );
 };
