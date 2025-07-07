32a0b811ece14bb26456f55517dbc90fce4fe029
Atik Mazumdar <rjatik990@gmail.com>
2025-07-07 22:28:43 +0600

    footer home added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import Home from './components/home/Home'
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/Footer'
+import CondominiumForm from './components/condominum-info/CondominiumForm'
+import PropertyAddress from './components/condominum-info/modals/PropertyAddress'
 
 function App() {
   
@@ -10,7 +12,10 @@ function App() {
   return (
     <div>
     <Navbar/>
-    <Home />
+    {/* <Home /> */}
+    <CondominiumForm />
+    <PropertyAddress/>
+    <Footer />
     </div>
   )
 }
